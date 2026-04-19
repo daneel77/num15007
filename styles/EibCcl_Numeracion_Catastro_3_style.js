3d88b0aa5c34a804144f23490b174fd399f3651f
daneel77 <83504647+daneel77@users.noreply.github.com>
2026-04-19 19:45:05 +0200

    Add files via upload

--- styles/EibCcl_Numeracion_Catastro_3_style.js
+++ styles/EibCcl_Numeracion_Catastro_3_style.js
@@ -8,25 +8,63 @@ var style_EibCcl_Numeracion_Catastro_3 = function(feature, resolution){
     };
     
     var labelText = ""; 
-    var value = feature.get("ENH_N1_00");
-    var labelFont = "13.0px \'Open Sans\', sans-serif";
-    var labelFill = "#323232";
-    var bufferColor = "#ffffff";
+    var valueNUM = feature.get("END_N1_00");
+    var valueLETRA = feature.get("END_L1_00");
+    var valueTIPO = feature.get("EN1_TIPO");
+    var valueMODIF = feature.get("ID_MODIF");
+    if (valueNUM == null) { 
+       valueNUM = ""; 
+    }
+    if (valueLETRA == null) { 
+       valueLETRA = ""; 
+    }
+    var labelFont = "16px, sans-serif";
+    var labelFill = "#000000";
+    var circleFill = "#00ff55";
+    var bufferColor = "#00ffff";
+    if (valueTIPO == "00_N1") {
+    	circleFill = "#00aa00";
+    }
+    if (valueMODIF == "MANTIENE EL ACTUAL") {
+    	bufferColor = "#ffffff";
+    }
+    if (valueMODIF == "DISTINTOS") {
+    labelFill = "#ffffff";
+    bufferColor = "#000000";
+    }
+    if (valueMODIF == "AÑADIDO") {
+    	bufferColor = "#ff0000";
+    }
     var bufferWidth = 5;
     var textAlign = "left";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
     if ("" !== null) {
-        labelText = String(value);
+        labelText = String(valueNUM + valueLETRA);
     }
-    var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(225,89,137,1.0)'})}),
+    
+        var style = [ 
+        new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: 10,
+            displacement: [10, -3],
+            stroke: new ol.style.Stroke({
+            color: circleFill,
+            width: 3.5
+        })
+        })
+    }),
+    new ol.style.Style({
         text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
-
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+    
+    
+    
+    /*var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;*/
     return style;
 };
